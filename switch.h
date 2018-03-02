9ad6905767514a09aaab337b5ea5293eab0203b4
Lmachii <lmachii@hawaii.edu>
2018-03-01 16:50:05 -1000

    Update switch.h

--- switch.h
+++ switch.h
@@ -1,55 +1,13 @@
-/*TO EDIT NOT FINAL!!!*/
+/*
+ * switch.h
+ */
 
-#define NUMSWITCHS 100
-#define NUMSWITCHLINKS 200
+struct switch_fwd {
+	int valid;	// validity flag
+	char dst;	// destination host ID
+	int port;	// port # (0-3)
+};
 
-typedef struct pqnode {
-   packetBuffer packet;
-   int k;
-   struct pqnode * next;
-} pqnode;
+void switch_main(int switch_id);
 
-typedef struct {
-   pqnode * head;
-   pqnode * tail;
-} pqueue;
 
-typedef struct {
-   int toHost[2];
-   int fromHost[2];
-} switchLink;
-
-typedef struct { /* State of host */
-   int   physid;              /* physical id */
-   int   rcvflag;
-   int   numlinks;
-   packetBuffer sendPacketBuff;  /* send packet buffer */
-   packetBuffer rcvPacketBuff;
-   switchLink sLink;
-   LinkInfo linkin[NUMSWITCHLINKS];           /* Incoming communication link */
-   LinkInfo linkout[NUMSWITCHLINKS];          /* Outgoing communication link */
-} switchState;
-
-typedef struct {
-   int numlinks;
-   switchLink link[NUMSWITCHLINKS];
-} switchLinkArrayType;
-
-typedef struct {
-   int valid;
-   int destNetworkAddress;
-   LinkInfo outlink;
-} tableEntry;
-
-typedef struct {
-   int numentries;
-   tableEntry Entry[NUMSWITCHLINKS];
-} forwardingTable;
-
-void switchMain(switchState * sstate);
-
-void switchInit(switchState * sstate, int physid);
-
-pqueue * init(void);
-pqnode * pop(pqueue * q);
-void push(pqueue * q,packetBuffer p,int k);
